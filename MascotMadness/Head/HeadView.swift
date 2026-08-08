c17b86fdfbee2688c89f16076fc8eee7b93b2b46
T Krobot <tkrobot@sabook016.local>
2026-08-08 11:16:54 +0800

    Update Head view with emoji face design

--- MascotMadness/Head/HeadView.swift
+++ MascotMadness/Head/HeadView.swift
@@ -3,12 +3,27 @@ import SwiftUI
 var head: some BodyPart {
     Head {
         ZStack {
+           
             Circle()
                 .frame(width: 200, height: 200)
-                .foregroundStyle(.yellow)
+                .foregroundStyle(.red)
+
+            Text("⭐️  ⭐️")
+                .font(.system(size: 55))
+                .offset(y: -15)
+
+            Text("🌙")
+                .font(.system(size: 35))
+                .offset(y: 55)
+            Text("🇸🇬")
+                .font(.system(size:40))
+
+            
+                
         }
     }
 }
+
 
 #Preview(traits: .fixedLayout(width: 200, height: 200)) {
     head
