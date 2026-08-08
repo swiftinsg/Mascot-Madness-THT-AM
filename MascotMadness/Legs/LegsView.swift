7cca760d06e8629c649a34105f24771b096f8522
Wingru <qingru.xu12@rafflesgirlssch.edu.sg>
2026-08-08 11:18:39 +0800

    Add left and right legs below the torso circle

--- MascotMadness/Legs/LegsView.swift
+++ MascotMadness/Legs/LegsView.swift
@@ -2,14 +2,27 @@ import SwiftUI
 
 var legs: some BodyPart {
     Legs {
-        ZStack {
+        VStack(spacing: 0) {
             Circle()
                 .frame(width: 200, height: 200)
                 .foregroundStyle(.yellow)
+
+            HStack(spacing: 0) {
+                Image("leftLeg")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 200)
+
+                Image("rightLeg")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 200)
+            }
+            .frame(width: 200)
         }
     }
 }
 
-#Preview(traits: .fixedLayout(width: 200, height: 200)) {
+#Preview(traits: .fixedLayout(width: 300, height: 400)) {
     legs
 }
