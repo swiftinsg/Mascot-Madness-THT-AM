3b3fbcacc9b2e2f6d00a0da3a54dae6f8144c958
Paviesh <sundaram.paviesh@sji.edu.sg>
2026-08-08 11:11:07 +0800

    Update torso to use Torso image asset

--- MascotMadness/Body/BodyView.swift
+++ MascotMadness/Body/BodyView.swift
@@ -3,44 +3,8 @@ import SwiftUI
 var torso: some BodyPart {
     Torso {
         ZStack {
-            
-            // 2. The Jacket and Shirt layout combined together
-            HStack(spacing: 0) {
-                // Left Jacket panel
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: 45, height: 200)
+            Image("Torso")
                 
-                // White T-Shirt (Center Layer)
-                ZStack {
-                    Rectangle()
-                        .fill(.white)
-                        .frame(width: 110, height: 200)
-                    
-                    // "I ❤️ SG" Shirt Graphics
-                    VStack(spacing: 8) {
-                        HStack(spacing: 6) {
-                            Text("I")
-                                .font(.system(size: 24, weight: .bold))
-                                .foregroundStyle(.black)
-                            
-                            Image(systemName: "heart.fill")
-                                .font(.system(size: 22))
-                                .foregroundStyle(.red)
-                        }
-                        
-                        Text("SG")
-                            .font(.system(size: 28, weight: .black))
-                            .foregroundStyle(.black)
-                    }
-                }
-                
-                // Right Jacket panel
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: 45, height: 200)
-            }
-            .frame(width: 200, height: 200)
         }
     }
 }
